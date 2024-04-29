802bacf221f381360e45f0839c28ee844428dead
Warren Uhrich <hello@warren.codes>
2024-04-29 15:01:03 -0600

    Add in-class/in-session example(s)

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 * [ ] Learning Cycle
 * [ ] Problem Solving
 
+## The Learning Cycle
+
+1. Build a Dictionary of Terms
+    * In your own words, after activities/lectures define the terms we're using
+    * Variable: a value we can use in our code, can be of various types...
+2. Read your code... "What is this? What happens next?"
+    * Anything not making sense? Add to the dictionary!
+3. Create a Cheatsheet for Patterns you identify (dictionary!)
+    * Write down examples of common patterns we see
+    * Write notes with these examples to understand what is dynamic
+4. Solve Problems (Practice)
---
+++ problem-solving-example.js
@@ -0,0 +1,35 @@
+// Build a function that adds an array of numbers and gives us the sum.
+
+/**
+ * 1) Identify INPUT -> Array(Numbers)
+ * 2) Identify OUTPUT -> Sum -> Number
+ * 3) Plan the PROCESSING
+ */
+
+
+/**
+ * Sum array of numbers.
+ * 
+ * @param {Array<Number>} numArr - Array of numbers.
+ * @return {Number} - Sum of all numbers in the array.
+ */
+const sumArrOfNums = function(numArr) {
+    let sum = 0;
+
+    // PROCESSING PLAN:
+    // 1) Iterate through the array
+    for(let i = 0; i < numArr.length; i++) {
+        // 2) Add number to our sum
+        const num = numArr[i];
+        sum = sum + num;
+    }
+    
+
+    return Number(sum);
+}
+
+const exp1 = sumArrOfNums([1, 2, 3]);
+console.log('Expect:', 6, 'Got:', exp1);
+
+const exp2 = sumArrOfNums([2, 2, 3]);
+console.log('Expect:', 7, 'Got:', exp2);
